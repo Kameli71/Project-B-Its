3fa56f16ec22890c8df63fee9cca7ac23c4eac6e
edizbak_its <jvalla@thenuumfactory.fr>
2023-08-29 14:44:41 +0200

    nettoyage structure

--- microservices/Gestion_API/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-# print(app.config.SQLALCHEMY_DATABASE_URI)
