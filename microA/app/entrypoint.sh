1c158183b73cc2c293d72c83a68c1da000f4425d
Nasim <Nasimhaidari371@gmail.com>
2023-09-07 14:20:38 +0200

    update Jenkinsfile

--- microA/app/entrypoint.sh
+++ microA/app/entrypoint.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-# restart_process() {
-#     echo "Redémarrage du processus principal..."
-# }
-
-# terminate() {
-#     echo "Arrêt du conteneur..."
-#     exit 0
-# }
-
-# trap 'terminate' SIGTERM
-# restart_process
-
-# while true; do
-#     sleep 1
-# done
 gunicorn -b 0.0.0.0:5000 app:app
