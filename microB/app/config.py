b4ec8f1e92db2789149d403ce5db4aeb192f17ac
edizbak_its <jvalla@thenuumfactory.fr>
2023-09-07 17:27:34 +0200

    tentative modif microB pour récup clef API via gestion API

--- microB/app/config.py
+++ microB/app/config.py
@@ -1,4 +1,4 @@
-import os
+import os, request
 from flask import Flask
 from decouple import config
 
@@ -11,3 +11,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 PRESTASHOP_API_URL = config('API_URL')
 PRESTASHOP_API_KEY = config('API_KEY')
+port_gestion = "5151"
+url_gestion = f"localhost:{port_gestion}/ws"
+
+api_tmp = request.get(f'{url_gestion}/is_ws_enabled', timeout=10, verify=False)
+while not bool(api_tmp.content):
+    api_tmp = request.get(f'{url_gestion}is_ws_enabled', timeout=10, verify=False)
+    sleep(10)
+try:
+    api_key_response = request.get(f"{url_gestion}/get_api_key/micro_b")
+    if api_key_response.ok:
+        api_key = api_key_response.text
+except Exception as e:
+    raise
+else:
+    pass
+finally:
+    pass
